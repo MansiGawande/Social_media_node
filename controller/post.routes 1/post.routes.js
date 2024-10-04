00aaf97d26ee0fabadc0757476a4b360c6f83506
Mansi Gawande <mansi.gawande@softude.com>
2024-10-04 20:13:41 +0530

    10-oct content

--- controller/post.routes 1/post.routes.js
+++ controller/post.routes 1/post.routes.js
@@ -1,3 +1,6 @@
+
+
+// backend pe upload ke waqt image ko resize karna hoga. Yeh ensure karega ki apne backend se aayi hui image fix size me ho without css ui par show ho ske  npm i sharp .sharp  images ko resize, compress aur format conversion karne ki suvidha deta hai.
 import multer from "multer";
 import path from "path";
 import express from "express";
@@ -20,17 +23,17 @@ const storage = multer.diskStorage({
         cb(null, uniqueSuffix + path.extname(file.originalname)); 
     }
 });
-
 const limits = {
     fileSize: 1024 * 1024 * 5
   };
   
 const upload = multer({ storage: storage
-     ,limits: limits }); // create multer instance
+     ,limits: limits
+     }); 
 
 const router = express.Router();
 router.post("/uploadps", upload.single("media_url"), createPost);
 router.get("/profileposts",profileposts);
-router.get("/postContent",postContent); 
+router.get("/postContent",postContent)
 
 export default router;
