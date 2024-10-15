0c05a4f65f1c67663eaf3d841b7d0d5093144535
Mansi Gawande <mansi.gawande@softude.com>
2024-10-15 19:05:01 +0530

    update

--- routes/follow.routes.js
+++ routes/follow.routes.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { addFollow,unfollow } from "../controller/follow.controller.js";
+import { addFollow,unfollow,following } from "../controller/follow.controller.js";
 
 const router = express.Router();
 router.post("/addFollow",addFollow);
 router.delete("/removefollow",unfollow)
+router.get("/following",following);
+
 export default router;
